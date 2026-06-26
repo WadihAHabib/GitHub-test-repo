a74daafecb423fd7a7bd09bef158df2f9724b150
WadihAHabib <wadih.abouhabib.FT@gmail.com>
2026-06-26 16:20:30 +0300

    Add indication for weather data display location
    Added a print statement to indicate where weather information will be displayed.

--- weather.py
+++ weather.py
@@ -2,4 +2,7 @@ def check_weather(city):  # Defines a function that accepts a city as a paramete
 
     print("Checking weather for", city)  # Displays a message indicating which city's weather is being checked
 
+
+    print("Weather data will appear here.") # Indicates where weather information will eventually be displayed
+
 check_weather("Paris")  # Calls the function and passes "Paris" as the city
