d4618d96947b98674085b0530b6f11af1fd927a3
WadihAHabib <Wadih.abouhabib.ft@gmail.com>
2026-06-29 17:43:54 +0300

    Update weather service message

--- weather.py
+++ weather.py
@@ -3,6 +3,9 @@ def check_weather(city):  # Defines a function that accepts a city as a paramete
     print("Checking weather for", city)  # Displays a message indicating which city's weather is being checked
 
 
-    print("Weather data will appear here.") # Indicates where weather information will eventually be displayed
+     # print("Weather data will appear here.") # Indicates where weather information will eventually be displayed
+
+    print("Connecting to weather service...") # Updated message from above
+
 
 check_weather("Paris")  # Calls the function and passes "Paris" as the city
